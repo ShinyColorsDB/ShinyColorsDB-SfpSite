86c90ec2ac1896f75b9d8651002c26b1129c6ca1
Euphokumiko <h.k.h94539@gmail.com>
2024-01-17 10:28:04 +0800

    fixed ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes, withEnabledBlockingInitialNavigation()),
+    provideClientHydration(withHttpTransferCacheOptions({})),
+    provideHttpClient(withFetch())
+  ]
 };
